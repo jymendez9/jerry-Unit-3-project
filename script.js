13353f62fde5458655f0234d34925c25892458d0
JerryGonzalez4 <18332761-JerryGonzalez4@users.noreply.replit.com>
2023-04-18 13:16:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let enter = document.querySelector(".Enter-buttons");
+let move = document.querySelector(".move on-buttons");
+let still = document.querySelector(".still-buttons");
+let quit = document.querySelector(".quit-buttons");
+let crowbar = document.querySelector(".crowbar-buttons");
+let leave = document.querySelector(".leave-buttons");
+let light = document.querySelector(".option-one-screen");
+
+
+
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
